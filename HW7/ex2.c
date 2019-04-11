c5da254283c2027334d5fef278d781dcbb5a58e8
Guillermo Schneider <guillermocharlesschneider@gmail.com>
2019-04-11 16:56:06 -0400

    part2

---
+++ HW7/ex2.c
@@ -0,0 +1,20 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, c;
+
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &r);
+  r=((r/25.4f));
+  a = PI * r * 2;
+  c = PI * r * r;
+
+  
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  printf("Its circumference is %3.2f (in)", c);
+}
