d5223c8ef174299910167046d17681d1921bdea8
Guillermo Schneider <guillermocharlesschneider@gmail.com>
2019-05-10 03:04:50 -0400

    update ll_equal.c

--- HW7/ll_equal.c
+++ HW7/ll_equal.c
@@ -7,7 +7,7 @@ typedef struct node {
 
 /* FIXME: this function is buggy. */
 int ll_equal(const node* a, const node* b) {
-	while (a != NULL) {
+	while (a && b) {
 		if (a->val != b->val)
 			return 0;
 		a = a->next;
